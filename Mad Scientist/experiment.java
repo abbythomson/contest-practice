e778e8fe720ed725e2bd3127d9a05b10c00f08e8
abigailthomson13 <abigailthomson13@augustana.edu>
2014-09-09 16:38:57 -0500

    almost complete

--- Mad Scientist/experiment.java
+++ Mad Scientist/experiment.java
@@ -1,13 +1,33 @@
-import java.io.*; 
+import java.io.*;  
 import java.text.*;
 import java.util.*;
-public class experiment {
+ public class experiment {
 
 	public static void main(String[] args) throws FileNotFoundException{
-		Scanner input = new Scanner(new File("results.in"));
-		String experimentResults=input.nextLine();
-	
+		Scanner input = new Scanner(new File("madscientist.in"));
+		int numK = input.nextInt();
+		while(numK!=0){
+			int[] resultsArray=new int[numK];
+			int[] exResults=new int[26];
+			for(int i=0;i<numK;i++){
+				resultsArray[i]=input.nextInt();
+				}
+			for (int i=0; i < resultsArray[0]; i++)
+			{
+				System.out.print("1 ");
+			}
+			for(int j=1;j<numK;j++){
+				int tester=resultsArray[j]-resultsArray[j-1];
+				if(tester==0)
+					continue;
+				for(int y=0;y<tester;y++){
+					System.out.print(j+1);
+					System.out.print(" ");				
+				}
+			}
+			System.out.println("a");
+			numK=input.nextInt();	
+		}
 		
 	}
-
 }
